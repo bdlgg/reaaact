c7cff4e55fa66cc19fbf39d76f055a91dc5450f1
aleksejkonstantinov <40akka@gmail.com>
2025-11-10 15:22:23 +0300

    19 practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
 import Greeting from "./Greeting.jsx";
+import UserCard from "./UserCard.jsx";
+import TaskList from "./TaskList.jsx";
+import Counter from "./Counter.jsx";
 
 function App() {
     return (
         <div className="App">
             <Greeting />
+            <UserCard name="Антон" role="Администратор" avatarURL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfVMhpKmVy_-iwfRLAiNiaDslMa-2oEz7KTw&s" isOnline={true}/>
+            <UserCard name="Леха" role="Сомелье" avatarURL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfVMhpKmVy_-iwfRLAiNiaDslMa-2oEz7KTw&s" isOnline={true}/>
+            <UserCard name="Саня" role="Экономист" avatarURL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfVMhpKmVy_-iwfRLAiNiaDslMa-2oEz7KTw&s" isOnline={false}/>
+            <TaskList />
+            <Counter />
         </div>
     );
 }
--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,7 +1,19 @@
 function Greeting() {
+    const userName = "Дружище"
+    const currentHour = new Date().getHours();
+    let timeOfDay;
+    if (currentHour < 12) {
+        timeOfDay = "Доброе утро";
+    }
+    else if (currentHour < 18) {
+        timeOfDay = "Добрый день";
+    }
+    else {
+        timeOfDay = "Добрый вечер";
+    }
     return (
         <div className="greeting">
-            <h1>Привет</h1>
+            <h1>{timeOfDay},  {userName}!</h1>
             <p>Рады видеть вас в нашем приложении</p>
         </div>
     )
