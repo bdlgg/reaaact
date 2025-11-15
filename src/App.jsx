d3324387cb50ce464e486a722383647b48731f34
aleksejkonstantinov <40akka@gmail.com>
2025-11-15 19:24:05 +0300

    19-20 practice

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ function App() {
         {id: 1, title: 'React Components', description: 'Изучение базовых компонентов', status: 'completed', notes: ''},
         {id: 2, title: 'JSX Syntax', description: 'Освоение синтаксиса JSX', status: 'in-progress', notes: ''},
         {id: 3, title: 'State Management', description: 'Работа с состоянием компонентов', status: 'not-started', notes: ''},
+        {id: 4, title: 'Postgres', description: 'Изучение работы с запросами в postgres', status: 'in-progress'},
+        {id: 5, title: 'useEffect', description: 'Работа с хуком useState', status: 'not-started'}
     ]);
     const [filter, setFilter] = useState('all');
 
